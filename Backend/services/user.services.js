2d2a0743b0eeec8ffcfb1659357c0ef99a8e5ca1
sagarDevHub <gullasagar9933@gmail.com>
2025-05-02 21:47:31 +0530

    User service successfully completed

--- Backend/services/user.services.js
+++ Backend/services/user.services.js
@@ -1,20 +1,31 @@
 import userModel from '../models/user.model.js';
 
 const createUser = async ({ firstname, lastname, email, password }) => {
-  if (!firstname || !email || !password) {
-    throw new Error(`All fields are required`);
+  try {
+    if (!firstname || !email || !password) {
+      const error = new Error('All fields are required');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const user = userModel.create({
+      fullname: {
+        firstname,
+        lastname: lastname || '',
+      },
+      email,
+      password,
+    });
+
+    return user;
+  } catch (error) {
+    if (error.code === 11000 && error.keyPattern.email) {
+      const err = new Error('Email already exists');
+      err.statusCode = 409;
+      throw err;
+    }
+    throw error;
   }
-
-  const user = userModel.create({
-    fullname: {
-      firstname,
-      lastname,
-    },
-    email,
-    password,
-  });
-
-  return user;
 };
 
 export default { createUser };
